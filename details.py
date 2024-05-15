4c52f18368802bfb28c0cc32d8972aa5e7c90f48
StubbornDEvil <70991755+Rohanthakur360@users.noreply.github.com>
2024-05-15 18:23:41 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 28919553
-api_hash = "2ed4532cc8353ab77179cb291ffeb86e"
-bot_token = "7034513055:AAHk6SoPwWMiGfc5knRllgN5QH5j5EvI07E"
+api_id = 28afafafa553
+api_hash = "2afafaffeb86e"
+bot_token = "70fafafafafvI07E"
 
